af3463c05e950d2bb8b20bb31181dae3d70418e2
Abhilash S <abhicse002@gmail.com>
2021-04-29 18:15:26 +0530

    added proxy class

--- src/movietime/videos/admin.py
+++ src/movietime/videos/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Videos,VideoProxy
+from django.utils.html import format_html
 
-admin.site.register(Videos)
-admin.site.register(VideoProxy)
+from .models import Videos,VideoProxy,VideoProxyforEdu
+
+
+@admin.register(Videos)
+class VideoAdmin(admin.ModelAdmin):
+    pass;
+
+@admin.register(VideoProxy)
+class VideoProxyAdmin(admin.ModelAdmin):
+    list_display = ('videos_id','title' , 'description','pk','length_in_min')
+    list_filter = ['title']
+    search_fields = ['title']
+
+    class Meta:
+        model = VideoProxy
+
+    def get_queryset(self, request):
+        return VideoProxy.objects.filter(length_in_min=120)
+
+@admin.register(VideoProxyforEdu)
+class VideoProxyAdminforEdu(admin.ModelAdmin):
+    list_display = ('videos_id','title' , 'description','pk','length_in_min')
+    list_filter = ['title']
+    search_fields = ['title']
+
+    class Meta:
+        model = VideoProxyforEdu
+
+    def get_queryset(self, request):
+        res =  VideoProxy.objects.filter(title="edu videos")
+        return res
+
+
+
+
+
+
--- src/movietime/videos/models.py
+++ src/movietime/videos/models.py
@@ -6,9 +6,24 @@ class Videos(models.Model):
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True,null=True)
     videos_id = models.CharField(max_length=100)
+    length_in_min = models.IntegerField(default=120)
+    class Meta:
+        #allows order by wrt to each column
+        ordering = ('videos_id','title' , 'description','pk')
+
+#we can change the way it displays the data in admin site
+    def __str__(self):
+        return  f'{self.title} , {self.description}'
 
 class VideoProxy(Videos):
     class Meta:
         proxy = True
-        verbose_name = "Publisehd v"
+        verbose_name = "Videos with length 120 minutes"
+        verbose_name_plural = "published Videos"
+
+class VideoProxyforEdu(Videos):
+    class Meta:
+        proxy = True
+        verbose_name = "Edu Videos"
+        verbose_name_plural = "Educational Video"
 
